a37ce7e4f3dcb28a3ed34c128e7901786f80c535
yonguk <dyddnr446@gmail.com>
2025-07-15 11:00:39 +0900

    치킨 먹고싶다

---
+++ NODEJS/004_ARRAY/010_avg.js
@@ -0,0 +1,13 @@
+const input = require('fs').readFileSync(0,"utf-8").toString().trim().split(/\r?\n/);
+const nums = input[1].split(" ").map(Number);
+const max_num = Math.max(...nums);
+
+const new_nums = nums.map((num)=>{
+    return num/max_num*100;
+});
+
+const new_avg = new_nums.reduce((prev,current)=>{
+    return prev+=current;
+}, 0) / nums.length;
+
+console.log(new_avg);
