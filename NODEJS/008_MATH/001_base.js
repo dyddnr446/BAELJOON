6ffbccc1a11ab0f955add9ee960fbdd62781c5a8
yonguk <dyddnr446@gmail.com>
2025-08-01 22:58:49 +0900

    /[0-9]/.test(1234) : 1234에 대한 정규 표현식 Math.pow(2,3) : 2의 3제곱

---
+++ NODEJS/008_MATH/001_base.js
@@ -0,0 +1,14 @@
+const input = require('fs').readFileSync(0,"utf-8").toString().trim().split(" ");
+const base = Number(input[1]);
+
+const nums = input[0].split("").map((v)=>{
+    if(/[A-Z]/.test(v))return v.charCodeAt()-55;
+    if(/[0-9]/.test(v))return v.charCodeAt()-48;
+}).reverse();
+
+let result = 0;
+for(let i=0;i<nums.length;i++){
+    result+=nums[i]*Math.pow(base,i)
+}
+
+console.log(result);
