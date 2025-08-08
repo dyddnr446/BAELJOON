c65baffdc41b4aae145aabb3e646fe4f71085993
yonguk <dyddnr446@gmail.com>
2025-08-08 14:25:13 +0900

    It's too dificult

---
+++ NODEJS/008_MATH/005_hive.js
@@ -0,0 +1,17 @@
+const input = Number(require('fs').readFileSync(0,"utf-8").toString().trim());
+
+function hive(num) {
+    if (num === 1) return 1;
+
+    let count = 1; 
+    let floor = 1;
+
+    while (true) {
+        count += floor * 6;
+        if (num <= count) {
+            return floor + 1;
+        }
+        floor++;
+    }
+}
+console.log(hive(input));
