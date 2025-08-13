49408c83026ec0943a9cb6b5325388b538035b7e
yonguk <dyddnr446@gmail.com>
2025-08-13 15:04:18 +0900

    Math

--- NODEJS/008_MATH/006_snail.js
+++ NODEJS/008_MATH/006_snail.js
@@ -3,6 +3,5 @@ const input = require('fs').readFileSync(0, "utf-8").trim().split(" ").map(Numbe
 const A = input[0];
 const B = input[1];
 const V = input[2];
-
 const days = Math.ceil((V - A) / (A - B)) + 1;
 console.log(days);
