e2ff5c24030f0a5401d190546847ebe537e7deec
yonguk <dyddnr446@gmail.com>
2025-08-24 13:53:55 +0900

    Loop

---
+++ NODEJS/009_MATH/006_pFactor.js
@@ -0,0 +1,14 @@
+const input = Number(require('fs').readFileSync(0,"utf-8").toString().trim());
+
+function test(num){
+    let i = 2;
+    while(num>=i){
+        if(num%i===0){
+            console.log(i);
+            num/=i;
+            continue;
+        }
+        i++;
+    }
+}
+test(input);
