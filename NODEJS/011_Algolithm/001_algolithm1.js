29f3ffbd6c6aed94ffd4014fe3fe7278ec44cb5b
yonguk <dyddnr446@gmail.com>
2025-08-27 14:14:39 +0900

    big O

---
+++ NODEJS/011_Algolithm/001_algolithm1.js
@@ -0,0 +1,4 @@
+const input = Number(require('fs').readFileSync(0,"utf-8").toString());
+
+console.log(input);
+console.log(0);
---
+++ NODEJS/011_Algolithm/003_algolithm3.js
@@ -0,0 +1,4 @@
+const input = Number(require('fs').readFileSync(0,"utf-8").toString());
+
+console.log(input ** 2);
+console.log(2);
