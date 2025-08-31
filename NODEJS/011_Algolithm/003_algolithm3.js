1aa8c423afed45db6cb7c5e4ac54d2a92a8807f2
yonguk <dyddnr446@gmail.com>
2025-08-31 13:41:00 +0900

    _

--- NODEJS/011_Algolithm/003_algolithm3.js
+++ NODEJS/011_Algolithm/003_algolithm3.js
@@ -1,5 +1,4 @@
-const input = Number(require('fs').readFileSync(0,"utf-8").toString());
-
-
-console.log(input ** 2);
+let n = Number(require('fs').readFileSync(0,"utf-8").toString());
+n-=1;
+console.log((1+n)*n/2);
 console.log(2);
