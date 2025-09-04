2bf50dfa98439b210dd37ebe63ccf03c9ccff117
yonguk <dyddnr446@gmail.com>
2025-09-04 16:50:33 +0900

    n*(n-1n)*(n-2n)/6

--- NODEJS/011_Algolithm/001_algolithm1.js
+++ NODEJS/011_Algolithm/001_algolithm1.js
@@ -1,4 +1,4 @@
 const input = Number(require('fs').readFileSync(0,"utf-8").toString());
 
-console.log(input);
+console.log(1);
 console.log(0);
---
+++ NODEJS/011_Algolithm/005_algolithm.js
@@ -0,0 +1,5 @@
+const n = BigInt(require('fs').readFileSync(0,"utf-8").toString());
+
+const result = n*(n-1n)*(n-2n)/6n;
+console.log(result.toString());
+console.log(3);
