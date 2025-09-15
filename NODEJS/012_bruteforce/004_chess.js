04a2d63033a7c2a33c2eb943778e4b8127a1bc89
yonguk <dyddnr446@gmail.com>
2025-09-15 22:06:16 +0900

    bf

--- NODEJS/012_bruteforce/004_chess.js
+++ NODEJS/012_bruteforce/004_chess.js
@@ -1,3 +1,23 @@
-// const input = require('fs').readFileSync(0, "utf-8").toString().split(/\r?\n/);
+const input = require('fs').readFileSync(0,"utf-8").toString().split(" ").map(Number);
 
-// mat = 
+function test(n1, n2){
+    let p = false;
+    let mat=[];
+    let raw=[];
+    for(let i=0;i<n2;i++){
+        for(let j=0;j<n1;j++){
+            if(!p){
+                raw.push("W");
+                p=!p;
+            }
+            else {
+                raw.push("B");
+                p=!p;
+            }
+        }
+        mat.push(raw);
+        rar=[];
+    }
+    console.log(mat);
+}
+test(input[0],input[1]);
