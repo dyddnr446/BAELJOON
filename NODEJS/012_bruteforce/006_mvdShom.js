47dc981dea26e7cec7401d71984fa3195ab7d2cd
yonguk <dyddnr446@gmail.com>
2025-09-22 07:59:06 +0900

    Shom

--- NODEJS/012_bruteforce/006_mvdShom.js
+++ NODEJS/012_bruteforce/006_mvdShom.js
@@ -2,6 +2,13 @@ const input = Number(require('fs').readFileSync(0,"utf-8").toString().trim());
 
 function test(n){
     let base = 666;
-    
+    let cnt = 1;
+    while(1){
+        if(cnt===n)return base;
+        base++;
+        if(String(base).indexOf('666')!=-1)cnt++;
+    }
 }
 
+const result = test(input);
+console.log(result);
