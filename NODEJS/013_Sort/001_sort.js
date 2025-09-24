8ed95f36e74ee08bd5eff61f4814464c3e29645c
yonguk <dyddnr446@gmail.com>
2025-09-24 14:36:44 +0900

    Sort

---
+++ NODEJS/013_Sort/001_sort.js
@@ -0,0 +1,21 @@
+const input = require('fs').readFileSync(0,"utf-8").toString().trim().split(/\r?\n/).map(Number);
+
+input.shift();
+
+function test(arr){
+    for(let i=0;i<arr.length;i++){
+        for(let j=i;j<arr.length;j++){
+            if(arr[i]>arr[j]){
+                let temp = arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log("---")
+const uniq_arr = [...new Set(input)];
+
+console.log(test(uniq_arr).join("\n"));
