137e42015ae81d810d7920fdb53e68eaebe62471
yonguk <dyddnr446@gmail.com>
2025-10-11 21:19:11 +0900

    Map

---
+++ NODEJS/014_Set/004_pocketmonMaster.js
@@ -0,0 +1,13 @@
+const input = require('fs').readFileSync(0,"utf-8").toString().trim().split(/\r?\n/);
+const [N, M] = input[0].split(" ").map(Number);
+const dict = input.slice(1,N+1);
+const ask = input.slice(N+1);
+
+const new_dict = new Map();
+dict.forEach((v,i)=>{
+    new_dict.set(v,i+1);
+});
+ask.forEach((v)=>{
+    if(!isNaN(Number(v)))console.log(dict[v-1]);
+    else console.log(new_dict.get(v));
+});
