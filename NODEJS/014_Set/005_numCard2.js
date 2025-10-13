342d4bede4e776cc33e0af7a675b82ce7991a1e4
yonguk <dyddnr446@gmail.com>
2025-10-13 10:32:45 +0900

    ||

---
+++ NODEJS/014_Set/005_numCard2.js
@@ -0,0 +1,19 @@
+const input = require('fs').readFileSync(0,"utf-8").toString().trim().split(/\r?\n/);
+
+const N = Number(input[0]);
+const have = input[1].split(" ").map(Number);
+const M = Number(input[2]);
+const check = input[3].split(" ").map(Number);
+
+const mHave = new Map();
+const result = [];
+
+for(const i of have){
+    mHave.set(i,(mHave.get(i) || 0) +1 );
+}
+
+check.forEach((v)=>{
+    result.push((mHave.get(v) || 0));
+});
+
+console.log(result.join(" "));
