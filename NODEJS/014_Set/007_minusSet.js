26f04170917ab7fbf8e427f9a65ed99e707bbf19
yonguk <dyddnr446@gmail.com>
2025-10-17 19:03:36 +0900

    Set

---
+++ NODEJS/014_Set/007_minusSet.js
@@ -0,0 +1,13 @@
+const input = require('fs').readFileSync(0,"utf-8").toString().trim().split(/\r?\n/);
+const A = new Set(input[1].split(" ").map(Number));
+const B = new Set(input[2].split(" ").map(Number));
+
+let result = 0;
+A.forEach((v)=>{
+    if(!B.has(v))result++;
+});
+B.forEach((v)=>{
+    if(!A.has(v))result++;
+});
+
+console.log(result);
