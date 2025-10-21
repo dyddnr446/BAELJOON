9d1e117494b215d6053e871a32bf7ae91b9508e7
yonguk <dyddnr446@gmail.com>
2025-10-21 11:39:28 +0900

    factor

---
+++ NODEJS/015_meaMul/003_fractMin.js
@@ -0,0 +1,27 @@
+const input = require('fs').readFileSync(0,"utf-8")
+    .toString().trim().split(/\r?\n/)
+    .map(v=>v.split(" ").map(Number));
+
+function gcd(a,b){
+    let r;
+    while(b!=0){
+        r = a%b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+
+const [A, B] = input[0];
+const [C, D] = input[1];
+
+const lcm = B * D /gcd(B,D);
+
+let boonza = (lcm/B)*A + (lcm/D)*C;
+let boonmo = lcm;
+
+const div = gcd(boonza,boonmo);
+boonza /= div;
+boonmo /= div;
+
+console.log(`${boonza} ${boonmo}`);
