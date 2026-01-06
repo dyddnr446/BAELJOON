26b1de39101cc5dceff58d9629f1515889677d37
yonguk <dyddnr446@gmail.com>
2026-01-06 14:32:53 +0900

    java 시작

---
+++ java/Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+
+       
+    public static void main(String[] args) {
+         //1번 문제
+        // System.out.println("Hello World!");
+
+        //2번 문제
+        Scanner scanner = new Scanner(System.in);
+        final String input[] = new String(scanner.nextLine()).split(" ");
+        System.out.println(Integer.parseInt(input[0]) + Integer.parseInt(input[1]));
+    }
+    
+
+}
