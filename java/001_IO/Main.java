b3142ca8001b7a46a9fc09706293ec6688e5e2a9
yonguk <dyddnr446@gmail.com>
2026-01-07 22:05:15 +0900

    _

---
+++ java/001_IO/Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+         //1번 문제
+        // System.out.println("Hello World!");
+
+        //2번 문제
+        // Scanner scanner = new Scanner(System.in);
+        // String input[] = scanner.nextLine().split(" ");
+        // int sum = Integer.parseInt(input[0]) + Integer.parseInt(input[1]);
+        // System.out.println(sum);
+
+        //3번 문제
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        System.out.println(A-B); 
+    }
+}
